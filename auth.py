de64c735216b17a8c1614f79d1ab2f954e92bbfa
Daniel Nielsen <daniel@norhoj.com>
2019-02-18 15:32:05 +0100

    OOf

--- auth.py
+++ auth.py
@@ -1,12 +1,20 @@
 import json, hashlib
 
 # Filenames
-DATA = "data.json"
+DATAFILE = "data.json"
 
 def login(username, password):
-	with open("data.json") as f:
-		data = json.loads(f.read())
-		if not username in data["users"]:
+	with open(DATAFILE) as f:
+		data = json.load(f)
+		users = data["users"]
+		user = {}
+
+		for info in users.values():
+			if info["username"] == username:
+				user = info
+		
+		if user == {}:
+			print(f"Username: {username} not found")
 			return False
 		
 		saltedpass = password+data["salt"]
@@ -15,8 +23,33 @@ def login(username, password):
 		print(saltedpass)
 		print(encryptedpass)
 
-		if data["users"][username]["password"] == encryptedpass:
+		if user["password"] == encryptedpass:
 			return True
 		
-		print("OOF")
 		return False
+	
+def register(username, password, email=None):
+	with open(DATAFILE) as f:  
+		data = json.load(f)
+		
+		for user in data["users"].values():
+			if user["username"] == username:
+				return 2 # 2 = Username already exist
+			elif user["email"] == email:
+				return 3 # 3 = Email already exist
+			
+		id = data["nextuser"]
+
+		data["users"][id] = {}
+		data["users"][id]["email"] = email
+		data["users"][id]["username"] = username
+
+		saltedpass = password+data["salt"]
+		encryptedpass = hashlib.sha256(saltedpass.encode()).hexdigest()
+		data["users"][id]["password"] = encryptedpass
+
+		data["nextuser"] = str(int(data["nextuser"])+1)
+
+		with open(DATAFILE, 'w') as outfile:  
+			json.dump(data, outfile, indent=4)
+			return 0
--- index.py
+++ index.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, session, redirect
-import auth
+import auth, json
 
 # Set up app
 app = Flask(__name__)
+app.secret_key = json.load(open("data.json"))["secret"]
 PORT = 8000
 
 @app.route('/')
@@ -15,6 +16,7 @@ def loginsite():
 		form = request.form.get
 		
 		if auth.login(form('uname'), form('pass')):
+			session['username'] = form('uname')
 			return "True :)"
 		return "False :("
 
@@ -22,7 +24,28 @@ def loginsite():
 
 @app.route('/register', methods=['GET', 'POST'])
 def registersite():
+	if request.method == 'POST':
+		form = request.form.get
+		
+		code = auth.register(form('uname'), form('pass'), form('email'))
+		if code == 0:
+			return "User created!<br><a href='/login'>Click here to go back</a>"
+		elif code == 2:
+			return "Username already in database."
+		elif code == 3:
+			return "Email already in database."
+		else:
+			return "Unknown error."
+
 	return render_template("register.html")
 
+@app.route('/me')
+def me():
+	return session.get('username', 'Not logged in')
+
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0', port=PORT)
+	app.run(
+		debug=True, host='0.0.0.0',
+		port=PORT,
+		#ssl_context='adhoc'
+	)
